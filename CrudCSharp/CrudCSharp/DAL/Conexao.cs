16a1ed033963cb219559b897140cb36ce89f0d0c
Lodi <andrew.lodi@hotmail.com>
2018-08-30 21:09:34 -0300

    Commit classe conexão.

--- CrudCSharp/CrudCSharp/DAL/Conexao.cs
+++ CrudCSharp/CrudCSharp/DAL/Conexao.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,25 @@ namespace CrudCSharp.DAL
 {
 	class Conexao
 	{
+		SqlConnection conexaoBD;
+
+		public Conexao()
+		{
+			conexaoBD = new SqlConnection();
+			conexaoBD.ConnectionString = "Data Source=localhost;Initial Catalog=CrudCSharpPessoasNoite;Integrated Security=True";
+		}
+
+		public SqlConnection Conectar()
+		{
+			if (conexaoBD.State == System.Data.ConnectionState.Closed)
+				conexaoBD.Open();
+			return conexaoBD;
+		}
+
+		public void Desconectar()
+		{
+			if (conexaoBD.State == System.Data.ConnectionState.Open)
+				conexaoBD.Close();
+		}
 	}
 }
